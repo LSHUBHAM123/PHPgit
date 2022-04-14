ae09b6df290eeb26a94592b4da40c96db81cb8e7
LSHUBHAM123 <shubham123bhilai@gmail.com>
2022-04-14 20:05:58 +0530

    used different file methods - fopen and fclose

---
+++ files.php
@@ -0,0 +1,12 @@
+<?php
+
+// $a = readfile("myfile.txt");     //will return file characters not count
+
+// echo $a;                 //readfile when given as assignment value,THEN
+                            //it reads file and also returns number of character read if echo is done
+
+// readfile("myfile.txt");   //it will directly return the text without counting the characters
+
+// readfile("MySql.php");  //can read any file and throws/outputs in output buffer
+readfile("file.html");
+?>
